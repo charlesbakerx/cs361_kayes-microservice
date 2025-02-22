6bd6c274e95eedad7b46895bb11cee164ccd8d20
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-02-21 19:08:05 -0600

    Adds registration and authentication functionality

--- auth.py
+++ auth.py
@@ -1,5 +1,34 @@
 """
-Handles user registration and authentication
+Handles user registration and authentication using bcrypt to hash passwords.
 WARNING: Currently stores users in a linux passwd style file in the form <user>:<hashed-password>
         I suggest coming up with a more secure option in the future.
 """
+
+import bcrypt
+PASSWD_FILE = "data/passwd"
+
+def register_user(username, passwd):
+    passwd_bytes = passwd.encode('utf-8')
+    hashed_passwd = bcrypt.hashpw(passwd_bytes, bcrypt.gensalt())
+
+    # Check that the user doesn't already exist.
+    with open(PASSWD_FILE, 'r') as f:
+        for line in f:
+            if line.startswith(username + ":"):
+                return False
+
+    # If it doesn't append the new user to the passwd file.
+    with open(PASSWD_FILE, 'a') as f:
+        f.write(f"{username}:{hashed_passwd}\n")
+
+def authenticate_user(username, passwd):
+    passwd_bytes = passwd.encode('utf-8')
+    with open(PASSWD_FILE, 'r') as f:
+        for line in f:
+            # Check that the user exists
+            if line.startswith(username + ":"):
+                # If it does then check the password return True for a match and False otherwise.
+                hashed_passwd = line.split(":")[1].strip().encode('utf-8')
+                return bcrypt.checkpw(passwd_bytes, hashed_passwd)
+    # Otherwise return False because the user was not found.
+    return False
