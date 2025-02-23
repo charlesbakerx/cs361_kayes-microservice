ee0524c5bd7b204e17ae534a69a8b3176452a13b
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-02-22 19:03:11 -0600

    Adds basic functio to generate and return a flask_jwt token

--- session_manager.py
+++ session_manager.py
@@ -1,3 +1,8 @@
 """
 Handles session management using JWT.
 """
+
+from flask_jwt_extended import create_access_token
+
+def generate_session_token(username):
+    return create_access_token(identity=username)
